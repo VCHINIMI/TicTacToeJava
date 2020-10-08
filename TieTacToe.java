db8c52d6334c55717b73e01e95ff743df3fb8f59
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-08 11:13:44 +0530

    [Vinay]Added : Added isWinner method to check winning conditions

--- TieTacToe.java
+++ TieTacToe.java
@@ -18,6 +18,28 @@ public class TieTacToe {
 			return Player.COMPUTER;
 	}
 
+//Display board feature
+	public static void displayBoard(char[] gameBoard) {
+		System.out.println("\n");
+		System.out.println(gameBoard[1] + "|" + gameBoard[2] + "|" + gameBoard[2]);
+		System.out.println("-----");
+		System.out.println(gameBoard[4] + "|" + gameBoard[5] + "|" + gameBoard[6]);
+		System.out.println("-----");
+		System.out.println(gameBoard[7] + "|" + gameBoard[8] + "|" + gameBoard[9]);
+	}
+	
+//Winning conditions
+	public static boolean isWinner(char[] board, char userCharacter) {
+		return (board[1] == userCharacter && board[2] == userCharacter && board[3] == userCharacter
+				|| board[4] == userCharacter && board[5] == userCharacter && board[6] == userCharacter
+				|| board[7] == userCharacter && board[8] == userCharacter && board[9] == userCharacter
+				|| board[1] == userCharacter && board[4] == userCharacter && board[7] == userCharacter
+				|| board[2] == userCharacter && board[5] == userCharacter && board[8] == userCharacter
+				|| board[3] == userCharacter && board[6] == userCharacter && board[9] == userCharacter
+				|| board[1] == userCharacter && board[5] == userCharacter && board[9] == userCharacter
+				|| board[3] == userCharacter && board[5] == userCharacter && board[7] == userCharacter);
+	}
+
 //Initialising Game Board	
 	public static char[] createGameBoard() {
 		char[] gameBoard = new char[10];
@@ -71,5 +93,10 @@ public class TieTacToe {
 		else
 			compInGameCharacter = 'X';
 		intitiateMove(gameBoard, getNextUserMove(gameBoard), userInGameCharacter);
+		for(int i = 0; i <4;i++) {
+			gameBoard[i]='X';
+		}
+		System.out.println(isWinner(gameBoard, 'X'));
+		displayBoard(gameBoard);
 	}
 }
