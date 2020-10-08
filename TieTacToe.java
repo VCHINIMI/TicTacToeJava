693d72683b694f67de0da4508302b1f160f84f9b
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-08 09:11:17 +0530

    [Vinay]Added : TicTacToe gameBoard creation

--- TieTacToe.java
+++ TieTacToe.java
@@ -1,7 +1,17 @@
 import java.util.Scanner;
 
 public class TieTacToe {
+	
+	public static char[] createGameBoard() {
+		char[] gameBoard = new char[10];
+		for(int index = 1; index < 10;index++) {
+			gameBoard[index] = ' ';
+		}
+		return gameBoard;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Program");
+		createGameBoard();
 	}
 }
