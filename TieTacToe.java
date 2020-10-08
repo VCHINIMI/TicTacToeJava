07c7a809bdb1b456a5eb0c1b9aefe40cc43d9a14
VCHINIMI <vinaychinimilli@gmail.com>
2020-10-08 09:26:06 +0530

    [Vinay]Added : Choose User Character

--- TieTacToe.java
+++ TieTacToe.java
@@ -1,17 +1,35 @@
 import java.util.Scanner;
 
 public class TieTacToe {
-	
+	public static Scanner userInputScanner = new Scanner(System.in);
+
+//Initialising Game Board	
 	public static char[] createGameBoard() {
 		char[] gameBoard = new char[10];
-		for(int index = 1; index < 10;index++) {
+		for (int index = 1; index < 10; index++) {
 			gameBoard[index] = ' ';
 		}
 		return gameBoard;
 	}
+
+//Choose inGameCharacter by USER
+	public static char userGameCharacter() {
+		System.out.println("Choose Character X or O");
+		char userCharacter = userInputScanner.next().charAt(0);
+		return userCharacter;
+	}
 	
+	//Main Method	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic Tac Toe Program");
 		createGameBoard();
+		char userInGameCharacter = userGameCharacter();
+		char compInGameCharacter = ' ';
+		if(userInGameCharacter == 'X')
+			compInGameCharacter = 'O';
+		else
+			compInGameCharacter = 'X';
+		System.out.println(userInGameCharacter+" "+compInGameCharacter);
+		
 	}
 }
